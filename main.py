d8a5d74e49e9622f5975ae1a6dd06bea8fce52b5
Заремба Данила <d4nilazaremba@yandex.ru>
2024-12-20 03:05:52 +0300

    Добавлен класс

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 class Coating():
-    colors = []
-    parts = []
+    colors = ["Белый", "Синий", "Желтый", "Красный", "Перламутровый", "Серый металлик"]
+    parts = ["Капот", "Передняя дверь", "Задняя дверь", "Передний бампер", "Задний бампер", "Крыша"]
+    def __init__(self):
+        self.part = None
+        self.color = None
+        self.price = None
+        self.cost = 0
     def __init__(self):
         pass
+    
     def get_info(self):
         pass
         
     def output_price(self):
         pass
+    
     def part_coefficient(self):
         pass
 

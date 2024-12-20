c334eab9d2f456ac34d490bb4b4318836d65eea2
Заремба Данила <d4nilazaremba@yandex.ru>
2024-12-20 03:20:01 +0300

    price

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class Coating():
         self.color = None
         self.price = None
         self.cost = 0
-<<<<<<< HEAD
+# <<<<<<< HEAD
     def __init__(self):
         pass
     
@@ -15,17 +15,42 @@ class Coating():
         
     def output_price(self):
         pass
-=======
+# =======
     def get_info(self):
         self.part = input(f"Введите название детали, Например {self.parts}: ")
         self.color = input(f"Введите цвет детали, Например: {self.colors}: ")
         
     def output_price(self):
         print(f"Цена покраски детали \"{self.part}\" в {self.color} составляет {self.price} рублей")
->>>>>>> feature/2
+# >>>>>>> feature/2
     
     def part_coefficient(self):
-        pass
+        color_coefficients = {
+            "Белый": 1,
+            "Синий": 1,
+            "Желтый": 1.1,
+            "Красный": 1,
+            "Перламутровый": 1.2,
+            "Серый металлик": 1.3
+        }
+
+        part_coefficients = {
+            "Капот": 1,
+            "Передняя дверь": 1.2,
+            "Задняя дверь": 1.1,
+            "Передний бампер": 1,
+            "Задний бампер": 1,
+            "Крыша": 1.1
+        }
+
+        base_price = 12000  
+
+
+        color_coef = color_coefficients.get(self.color, 1)
+        part_coef = part_coefficients.get(self.part, 1)
+
+
+        self.price = base_price * color_coef * part_coef
 
 if __name__ == '__main__':
     a1 = Coating()

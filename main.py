86932ef0a827e632a3d5fb82ed47ea4cff03063a
Заремба Данила <d4nilazaremba@yandex.ru>
2024-12-20 03:02:15 +0300

    создание шаблона кода

---
+++ main.py
@@ -0,0 +1,18 @@
+class Coating():
+    colors = []
+    parts = []
+    def __init__(self):
+        pass
+    def get_info(self):
+        pass
+        
+    def output_price(self):
+        pass
+    def part_coefficient(self):
+        pass
+
+if __name__ == '__main__':
+    a1 = Coating()
+    a1.get_info()
+    a1.part_coefficient()
+    a1.output_price()
